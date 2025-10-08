0a2dbbca5dc6bfa5e83258235e9c67bc2d22ad27
Abe Coutinho <abrcoutinho@gmail.com>
2025-10-08 17:35:26 -0300

    Dep15

--- public/js/smooth-scroll.js
+++ public/js/smooth-scroll.js
@@ -14,43 +14,46 @@
 
 
 // Momentum
-const storeApp = document.getElementById('smooth-wrapper');
-const storeContent = document.getElementById('smooth-content');
+if (!/Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+	// Executa o script apenas em desktops
+	const storeApp = document.getElementById('smooth-wrapper');
+	const storeContent = document.getElementById('smooth-content');
 
-let sx = 0, // For scroll positions
-    sy = 0;
-let dx = sx, // For container positions
-    dy = sy;
+	let sx = 0, // For scroll positions
+			sy = 0;
+	let dx = sx, // For container positions
+			dy = sy;
 
-storeApp.style.height = storeContent.clientHeight + 'px';
+	storeApp.style.height = storeContent.clientHeight + 'px';
 
-storeContent.style.position = 'fixed';
-storeContent.style.top = 0;
-storeContent.style.left = 0;
+	storeContent.style.position = 'fixed';
+	storeContent.style.top = 0;
+	storeContent.style.left = 0;
 
-// Bind a scroll function
-window.addEventListener('scroll', easeScroll);
+	// Bind a scroll function
+	window.addEventListener('scroll', easeScroll);
 
-function easeScroll() {
-  sx = window.pageXOffset;
-  sy = window.pageYOffset;
-}
+	function easeScroll() {
+		sx = window.pageXOffset;
+		sy = window.pageYOffset;
+	}
 
-window.requestAnimationFrame(render);
+	window.requestAnimationFrame(render);
 
-function render(){
-  //We calculate our container position by linear interpolation method
-  dx = li(dx,sx,0.04);
-  dy = li(dy,sy,0.04);
+	function render(){
+		//We calculate our container position by linear interpolation method
+		dx = li(dx,sx,0.04);
+		dy = li(dy,sy,0.04);
 
-  dx = Math.floor(dx * 100) / 100;
-  dy = Math.floor(dy * 100) / 100;
-  
-  storeContent.style.transform = `translate3d(-${dx}px, -${dy}px, 0px)`;
- 
-  window.requestAnimationFrame(render);
-}
+		dx = Math.floor(dx * 100) / 100;
+		dy = Math.floor(dy * 100) / 100;
+		
+		storeContent.style.transform = `translate3d(-${dx}px, -${dy}px, 0px)`;
+	 
+		window.requestAnimationFrame(render);
+	}
 
-function li(a, b, n) {
-  return (1 - n) * a + n * b;
+	function li(a, b, n) {
+		return (1 - n) * a + n * b;
+	}
 }
