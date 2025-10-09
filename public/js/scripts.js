bce5906aeeee62d914c4d1c5c964f1b51722edc7
Abe Coutinho <abrcoutinho@gmail.com>
2025-10-09 13:27:21 -0300

    Dep23

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,32 @@
+// Scroll event
+function isScrolled(e) {
+  const topbar = document.getElementById("topbar");
+  const scrolledDown = document.body.scrollTop > 1 || document.documentElement.scrollTop > 1;
+
+  if (scrolledDown) {
+    topbar.classList.add("scrolled");
+  }
+
+  if(!scrolledDown) {
+    topbar.classList.remove("scrolled");
+  }
+}
+window.addEventListener('scroll', isScrolled);
+
+
+// Show/hide navbar
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     navbar.style.top = "0";
+//   } else {
+//     navbar.style.top = "-220px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
+
 // Product details call
 document.addEventListener('DOMContentLoaded', function() {
   const openProductDetails = document.querySelectorAll('.open-product-details');
